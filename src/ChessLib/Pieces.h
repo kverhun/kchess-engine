09b2d861a55a2fa066d0c78fcd30b14536ed277c
kverhun <k.verhun@gmail.com>
2018-01-10 00:41:47 +0200

    Add Knight class

--- src/ChessLib/Pieces.h
+++ src/ChessLib/Pieces.h
@@ -14,4 +14,15 @@ namespace Chess
 		std::vector<TPosition> GetPossibleMoveTargets(
 			const TPosition& i_from, const Board& i_board) const override;
 	};
+
+	class Knight : public Piece
+	{
+	public:
+		Knight(EColor i_color);
+		~Knight() = default;
+
+		const std::string& GetString() const override;
+		std::vector<TPosition> GetPossibleMoveTargets(
+			const TPosition& i_from, const Board& i_board) const override;
+	};
 }
