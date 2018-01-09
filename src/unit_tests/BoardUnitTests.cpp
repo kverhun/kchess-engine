d54e12c47307382fc2517d0538d5a234f80c5ff4
kverhun <k.verhun@gmail.com>
2018-01-09 20:58:05 +0200

    Add Piece getter by position

--- src/unit_tests/BoardUnitTests.cpp
+++ src/unit_tests/BoardUnitTests.cpp
@@ -18,3 +18,34 @@ TEST_CASE("ShouldMovePieces")
         board.MakeMove({Chess::PositionFromString("e2"), Chess::PositionFromString("e4")});
     }
 }
+
+TEST_CASE("ShouldReturnPieceOnPosition")
+{
+	using namespace Chess;
+	SECTION("EmptyCellsOnStart")
+	{
+		Chess::Board board;
+		const auto piece_opt = board.GetPiece(PositionFromString("e3"));
+		for (char col = 'a'; col <= 'g'; ++col)
+			for (int row = 3; row <= 6; ++row)
+			{
+				const auto pos = PositionFromString(std::string(1, col) + std::to_string(row));
+				const auto piece_opt = board.GetPiece(pos);
+				REQUIRE(std::nullopt == piece_opt);
+			}
+	}
+	
+	SECTION("OccupiedCellsOnStart")
+	{
+		Chess::Board board;
+		const auto piece_opt = board.GetPiece(PositionFromString("e3"));
+		for (char col = 'a'; col <= 'g'; ++col)
+			for (int row : {1,2,7,8})
+			{
+				const auto pos = PositionFromString(std::string(1, col) + std::to_string(row));
+				const auto piece_opt = board.GetPiece(pos);
+				REQUIRE(piece_opt.has_value());
+			}
+	}
+
+}
