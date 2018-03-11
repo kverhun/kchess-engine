c4aaec90e921a91a7dca82f121449850a3c4a9c9
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-03-11 19:18:06 +0200

    Add rook impl

---
+++ src/ChessLib/Pieces/Rook.cpp
@@ -0,0 +1,61 @@
+#include "Pieces.h"
+
+#include "Board.h"
+#include "Common.h"
+
+namespace Chess
+{
+    Rook::Rook(EColor i_color)
+        : Piece(i_color)
+    {
+    }
+
+    const std::string& Rook::GetString() const
+    {
+        static const std::string g_rook_str = "R";
+        return g_rook_str;
+    }
+
+    std::vector<TPosition> Rook::GetPossibleMoveTargets(
+        const TPosition& i_from, const Board& i_board) const
+    {
+        std::vector<TPosition> targets;
+
+        auto check_and_append_pos = [&](const TPosition& i_pos) -> bool
+        {
+            if (!IsPositionOccupied(i_board, i_pos, GetColor()))
+            {
+                targets.push_back(i_pos);
+                return true;
+            }   
+            else
+                return false;
+        };
+
+        for (int i = i_from.first - 1; i >= 0; --i)
+        {
+            if (!check_and_append_pos({i, i_from.second}))
+                break;
+        }
+
+        for (int i = i_from.first + 1; i <= 7; ++i)
+        {
+            if (!check_and_append_pos({i, i_from.second}))
+                break;
+        }
+
+        for (int j = i_from.second - 1; j >= 0; --j)
+        {
+            if (!check_and_append_pos({i_from.first, j}))
+                break;
+        }
+
+        for (int j = i_from.second + 1; j <= 7; ++j)
+        {
+            if (!check_and_append_pos({i_from.first, j}))
+                break;
+        }
+
+        return targets;
+    }
+}
