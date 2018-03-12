b9dd09979d6e4c63547740c7e6eab0a77ed5670b
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-03-12 14:24:26 +0200

    Add pos to string implementation

--- src/unit_tests/CommonUnitTests.cpp
+++ src/unit_tests/CommonUnitTests.cpp
@@ -2,6 +2,7 @@
 
 #include <ChessLib/Common.h>
 #include <ChessLib/Board.h>
+#include <ChessLib/Piece.h>
 
 using namespace Chess;
 
@@ -22,3 +23,31 @@ TEST_CASE("IsPositionOccupied")
 
     }
 }
+
+TEST_CASE("PositionToStringTests")
+{
+    SECTION("PositionToStringInverse")
+    {
+        std::set<std::string> positions = {
+            "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",
+            "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
+            "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8"
+        };
+        for (const auto& str_pos : positions)
+        {
+            const auto pos = PositionFromString(str_pos);
+            const auto str_pos_converted = PositionToString(pos);
+            REQUIRE(str_pos == str_pos_converted);
+        }
+    }
+}
+
+TEST_CASE("GenerateRankTests")
+{
+    SECTION("GenerateRank-01")
+    {
+        Board board;
+        
+    }
+}
+
