697323a74778a5dc9699498bb2cfe4860535dadd
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-03-25 21:30:24 +0300

    impl parsing position from fen

--- src/ChessLib/ImportExportUtils.cpp
+++ src/ChessLib/ImportExportUtils.cpp
@@ -1,5 +1,6 @@
 #include "ImportExportUtils.h"
 
+#include <set>
 #include <sstream>
 
 using namespace Chess;
@@ -28,6 +29,29 @@ namespace
 
         return info;
     }
+
+    void _ParseFENRankToState(TState& io_state, int i_rank, const std::string& i_rank_str)
+    {
+        static const std::set<char> g_figure_chars = {'r', 'R', 'n', 'N', 'b', 'B', 'q', 'Q', 'k', 'K', 'p', 'P'};
+
+        int cur_file_idx = 0;
+        int cur_string_idx = 0;
+        while(cur_file_idx < 8)
+        {
+            const char c = i_rank_str[cur_string_idx];
+            if (g_figure_chars.count(c) > 0)
+            {
+                io_state.insert({TPosition{i_rank, cur_file_idx}, c});
+                ++cur_file_idx;
+            }
+            else
+            {
+                const int num_empty = c - '0';
+                cur_file_idx += num_empty;
+            }
+            ++cur_string_idx;
+        }
+    }
 }
 
 std::vector<std::string> Chess::IO::TokenizeString(const std::string& i_line, const char i_delimiter)
@@ -45,5 +69,11 @@ std::vector<std::string> Chess::IO::TokenizeString(const std::string& i_line, co
 TState Chess::IO::FENStringToState(const std::string& i_fen_string)
 {
     TState state;
+
+    const auto fen_info = _FENStringToFENInfo(i_fen_string);
+
+    for (int i = 0; i < 8; ++i)
+        _ParseFENRankToState(state, 8-i-1, fen_info.m_ranks[i]);
+
     return state;
 }
