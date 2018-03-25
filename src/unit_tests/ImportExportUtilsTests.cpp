e3c920ff6c953c842f3acb588a3c36e3f01cbb05
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-03-25 22:45:52 +0300

    add unit tests for FEN parsing

--- src/unit_tests/ImportExportUtilsTests.cpp
+++ src/unit_tests/ImportExportUtilsTests.cpp
@@ -34,4 +34,20 @@ TEST_CASE("ParseFENTest")
 
         REQUIRE(GetInitialPosition() == state);
     }
+
+    SECTION("Test01")
+    {
+        const std::string input_str = "8/8/8/8/8/8/8/8 w KQkq - 0 1";
+        const auto state = FENStringToState(input_str);
+        REQUIRE(state.empty());
+    }
+
+    SECTION("Test02")
+    {
+        const std::string input_str = "8/3p4/8/8/8/8/8/8 w KQkq - 0 1";
+        const auto state = FENStringToState(input_str);
+        REQUIRE(1 == state.size());
+        REQUIRE(state.count(TPosition{6,3}) == 1);
+        REQUIRE(state.at(TPosition{6,3}) == 'p');
+    }
 }
