9efdd1c97c45be637c6b9ab5ef33f77280d4098b
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-03-27 10:59:52 +0300

    add rook test  on empty board

--- src/unit_tests/RookUnitTests.cpp
+++ src/unit_tests/RookUnitTests.cpp
@@ -3,6 +3,7 @@
 #include <ChessLib/Board.h>
 #include <ChessLib/Game.h>
 #include <ChessLib/Pieces.h>
+#include <ChessLib/ImportExportUtils.h>
 
 #include <algorithm>
 
@@ -31,4 +32,26 @@ TEST_CASE("RookInitialPosition")
             REQUIRE(rook_targets.empty());
         }
     }
+
+    SECTION("TestOnEmptyBoard1")
+    {
+        std::map<std::string, std::string> positions_to_test = {
+            {"8/8/8/3R4/8/8/8/8 w KQkq - 0 1", "d5"},
+            {"8/8/8/1R6/8/8/8/8 w KQkq - 0 1", "b5"},
+            {"2R5/8/8/8/8/8/8/8 w KQkq - 0 1", "c8"},
+            {"8/8/8/8/8/8/7R/8 w KQkq - 0 1", "h2"},
+            {"8/8/8/R7/8/8/8/8 w KQkq - 0 1", "a5"}
+        };
+        
+        for (const auto& test_case : positions_to_test)
+        {
+            const std::string fen_string = test_case.first;
+            const auto rook_pos = PositionFromString(test_case.second);
+            Board board{IO::FENStringToState(fen_string)};
+            const auto& rook = board.GetPiece(rook_pos)->get();
+            const auto rook_move_targets = rook.GetPossibleMoveTargets(rook_pos, board);
+            REQUIRE(14 == rook_move_targets.size());
+        }
+    }
+
 }
