9bba8267486483e68e94147f5d5af0c6af390b9f
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-04-01 20:32:04 +0300

    Simplify letter_to_piece map

--- src/ChessLib/Pieces.cpp
+++ src/ChessLib/Pieces.cpp
@@ -1,22 +1,31 @@
 #include "Pieces.h"
 
+namespace
+{
+    template<typename T, Chess::EColor Color>
+    std::unique_ptr<T> _Create()
+    {
+        return std::make_unique<T>(Color);
+    }
+}
+
 namespace Chess
 {
     std::unique_ptr<Piece> CreatePieceFromSingleLetter(char i_letter)
     {
         static const std::map<char, std::function<std::unique_ptr<Piece>()>> g_letter_to_creator = {
-            { 'R', [](){return std::make_unique<Rook>(EColor::White);} },
-            { 'r', [](){return std::make_unique<Rook>(EColor::Black);} },
-            { 'N', [](){return std::make_unique<Knight>(EColor::White);}},
-            { 'n', [](){return std::make_unique<Knight>(EColor::Black);}},
-            { 'B', [](){return std::make_unique<Bishop>(EColor::White);}},
-            { 'b', [](){return std::make_unique<Bishop>(EColor::Black);}},
-            { 'Q', [](){return std::make_unique<Queen>(EColor::White);}},
-            { 'q', [](){return std::make_unique<Queen>(EColor::Black);}},
-            { 'K', [](){return std::make_unique<King>(EColor::White);}},
-            { 'k', [](){return std::make_unique<King>(EColor::Black);}},
-            { 'P', [](){return std::make_unique<Pawn>(EColor::White);}},
-            { 'p', [](){return std::make_unique<Pawn>(EColor::Black);}}
+            { 'R', _Create<Rook, EColor::White> },
+            { 'r', _Create<Rook, EColor::Black> },
+            { 'N', _Create<Knight, EColor::White> },
+            { 'n', _Create<Knight, EColor::Black> },
+            { 'B', _Create<Bishop, EColor::White> },
+            { 'b', _Create<Bishop, EColor::Black> },
+            { 'Q', _Create<Queen, EColor::White> },
+            { 'q', _Create<Queen, EColor::Black> },
+            { 'K', _Create<King, EColor::White> },
+            { 'k', _Create<King, EColor::Black> },
+            { 'P', _Create<Pawn, EColor::White> },
+            { 'p', _Create<Pawn, EColor::Black> }
         };
         return g_letter_to_creator.at(i_letter)();
     }
