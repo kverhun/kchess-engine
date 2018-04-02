767c5b5608392ff74c95f0ea92d23c95cf55d859
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-04-02 09:17:17 +0300

    use common logic in bishop move

--- src/ChessLib/Pieces/Bishop.cpp
+++ src/ChessLib/Pieces/Bishop.cpp
@@ -3,46 +3,9 @@
 #include "Board.h"
 
 #include <algorithm>
+#include <set>
 
 using namespace Chess;
-
-namespace
-{
-    TPosition _ApplyShift(const TPosition& i_pos, const TPosition& i_shift)
-    {
-        return TPosition{ i_pos.first + i_shift.first, i_pos.second + i_shift.second };
-    }
-
-    std::vector<TPosition> _GenerateDiag(const TPosition& i_pos, const TPosition& i_shift)
-    {
-        std::vector<TPosition> res;
-        res.push_back(i_pos);
-        while (Chess::IsPositionOnBoard(_ApplyShift(res.back(), i_shift)))
-            res.push_back(_ApplyShift(res.back(), i_shift));
-        return res;
-    }
-
-    std::vector<TPosition> _CutDiag(const std::vector<TPosition>& i_diag, const Board& i_board)
-    {
-        std::vector<TPosition> res = { i_diag.front() };
-
-        const auto our_color = i_board.GetPiece(i_diag.front())->get().GetColor();
-        for (auto i = 1u; i < i_diag.size(); ++i)
-        {
-            const auto cell_content = i_board.GetPiece(i_diag[i]);
-            if (cell_content)
-            {
-                if (cell_content->get().GetColor() != our_color)
-                    res.push_back(i_diag[i]);
-                break;
-            }
-            else
-                res.push_back(i_diag[i]);
-        }
-        return res;
-    }
-
-}
 
 namespace Chess
 {
@@ -61,21 +24,19 @@ namespace Chess
     std::vector<TPosition> Bishop::GetPossibleMoveTargets(
         const TPosition& i_from, const Board& i_board) const
     {
-        std::vector<TPosition> res;
-        for (TPosition shift : { TPosition{ 1,1 }, TPosition{ 1,-1 }, TPosition{ -1, 1 }, TPosition{ -1,-1 } })
-        {
-            const auto diag = _GenerateDiag(i_from, shift);
-            const auto cut_diag = _CutDiag(diag, i_board);
-            for (const auto& pos : cut_diag)
-                res.push_back(pos);
-        }
+        std::vector<TPosition> targets;
+        
+        std::set<TPosition> directions = {
+            TPosition{1, 1}, TPosition{1, -1}, TPosition{-1, 1}, TPosition{-1, -1}
+        };
 
-        res.erase(std::remove_if(res.begin(), res.end(),
-            [&](const TPosition& i_pos)
-            {
-                return IsPositionOccupied(i_board, i_pos, this->GetColor());
-            }),
-            res.end());
-        return res;
+        for (const auto direction : directions)
+        {
+            const auto positions = GoInDiretionWhilePossible(i_from, direction, i_board, GetColor());
+            for (const auto& pos : positions)
+                targets.push_back(pos);
+        }
+        
+        return targets;
     }
 }
