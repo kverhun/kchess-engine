5eec0f2e8de92c89b3b22f6e8c53e27d6d3f06ef
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-04-06 21:31:49 +0300

    run game

--- src/random_exp_app/main.cpp
+++ src/random_exp_app/main.cpp
@@ -9,13 +9,24 @@ using namespace Chess;
 
 namespace
 {
+    using EGameResult = Chess::GameManager::EGameResult;
+    std::string _GameResultToString(const EGameResult& i_res)
+    {
+        const static std::map<EGameResult, std::string> g_result_to_string = {
+            {EGameResult::WhiteWin, "White win"},
+            {EGameResult::BlackWin, "Black win"},
+            {EGameResult::Draw,     "Draw"}
+        };
+        return g_result_to_string.at(i_res);
+    }
+
     class _RandomMovePlayer : public IPlayer
     {
     public:
         _RandomMovePlayer(EColor i_color)
             : m_color(i_color)
         {}
-        
+
         Move GetMove(const Board& i_board)
         {
             const auto possible_moves = GetPossibleMoves(i_board, m_color);
@@ -34,7 +45,12 @@ namespace
 
 int main(int i_argc, char** ip_argv)
 {
-    std::cout << "Hello";
+    _RandomMovePlayer player_white{EColor::White};
+    _RandomMovePlayer player_black(EColor::Black);
+
+    const auto game_result = GameManager::PerformGame(player_white, player_black);
     
+    std::cout << "Result: " << _GameResultToString(game_result);
+
     return 0;
 }
