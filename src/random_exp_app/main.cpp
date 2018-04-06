3ca09d5b8e9f94b84fc33757c7cf695dce0580ac
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-04-06 21:28:52 +0300

    add simple random player impl

--- src/random_exp_app/main.cpp
+++ src/random_exp_app/main.cpp
@@ -1,8 +1,39 @@
+#include <ChessLib/Common.h>
+#include <ChessLib/IPlayer.h>
+#include <ChessLib/GameManager.h>
+
 #include <iostream>
+#include <random>
+
+using namespace Chess;
+
+namespace
+{
+    class _RandomMovePlayer : public IPlayer
+    {
+    public:
+        _RandomMovePlayer(EColor i_color)
+            : m_color(i_color)
+        {}
+        
+        Move GetMove(const Board& i_board)
+        {
+            const auto possible_moves = GetPossibleMoves(i_board, m_color);
+            
+            std::default_random_engine generator(0);
+            std::uniform_int_distribution<int> random(0, possible_moves.size());
+            const auto random_idx = random(generator);
+
+            return possible_moves[random_idx];
+        }
+
+    private:
+        const EColor m_color;
+    };
+}
 
 int main(int i_argc, char** ip_argv)
 {
-
     std::cout << "Hello";
     
     return 0;
