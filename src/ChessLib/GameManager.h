b229e07ccf624a4d4e68fa02adababefa191f926
Kostya Verhun <k.verhun@gmail.com>
2018-04-14 13:25:35 +0300

    Make game manager return more info (#18)
    * add gameinfo output for gamemanager
    
    * output gameinfo after random game
    
    * make logging for mass run
    
    * fixup timestamp generation

--- src/ChessLib/GameManager.h
+++ src/ChessLib/GameManager.h
@@ -17,11 +17,22 @@ namespace Chess
             BlackWin,
             Draw
         };
+        
+        struct GameInfo
+        {   
+            EGameResult m_result;
+            std::vector<Move> m_moves;
+            std::vector<TState> m_states;
+        };
 
         using TOptionalStream = std::optional<std::reference_wrapper<std::ostream>>;
+        using TOptionalGameInfo = std::optional<std::reference_wrapper<GameInfo>>;
         static EGameResult PerformGame(
             IPlayer& i_player_white, IPlayer& i_player_black,
             TOptionalStream i_log_summary = std::nullopt,
-            TOptionalStream i_log_verbose = std::nullopt);
+            TOptionalStream i_log_verbose = std::nullopt,
+            TOptionalGameInfo i_game_info = std::nullopt
+            );
+
     };
 }
--- src/random_exp_app/main.cpp
+++ src/random_exp_app/main.cpp
@@ -1,7 +1,9 @@
 #include <ChessLib/Common.h>
 #include <ChessLib/IPlayer.h>
+#include <ChessLib/ImportExportUtils.h>
 #include <ChessLib/GameManager.h>
 
+#include <chrono>
 #include <fstream>
 #include <functional>
 #include <iostream>
@@ -51,23 +53,40 @@ int main(int i_argc, char** ip_argv)
 {
     _RandomMovePlayer player_white{EColor::White};
     _RandomMovePlayer player_black(EColor::Black);
-    
+
+    const auto timestamp_str = std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()).count());
+
     std::unique_ptr<std::ostream> p_log_stream;
     auto verbose_stream = [&]() -> GameManager::TOptionalStream
     {
         if (i_argc > 1)
         {
             const std::string log_folder = ip_argv[1];
-            p_log_stream = std::make_unique<std::ofstream>(log_folder + "/log.txt");
+            p_log_stream = std::make_unique<std::ofstream>(log_folder + "/log" + timestamp_str + ".txt");
             return std::make_optional(std::ref(*p_log_stream.get()));
         }
         else
             return std::nullopt;
     }();
 
-    const auto game_result = GameManager::PerformGame(player_white, player_black, std::ref(std::cout), verbose_stream);
+    GameManager::GameInfo info;
+    const auto game_result = GameManager::PerformGame(player_white, player_black, std::nullopt, std::nullopt, 
+        std::make_optional((std::ref(info))));
     
-    std::cout << "Result: " << _GameResultToString(game_result);
+    std::cout << timestamp_str << ", " << _GameResultToString(game_result) << ", " << info.m_moves.size() / 2 << '\n';
+
+    if (i_argc > 1)
+    {
+        const std::string log_folder = ip_argv[1];
+        auto log_stream = std::ofstream(log_folder + "/log" + timestamp_str + ".txt");
+        log_stream << _GameResultToString(game_result);
+        for (const auto& state : info.m_states)
+            log_stream << IO::StateToFENString(state) << "\n";
+        log_stream << "\n";
+        for (const auto& move : info.m_moves)
+            log_stream << PositionToString(move.m_from) << ' ' << PositionToString(move.m_to) << '\n';
+    }
 
     return 0;
 }
